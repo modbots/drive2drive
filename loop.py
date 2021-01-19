7b45ac0279bbbb55384bcbf62f66328d373c2824
modbots <77435462+modbots@users.noreply.github.com>
2021-01-19 22:23:02 +0630

    Update loop.py

--- loop.py
+++ loop.py
@@ -1,5 +1,23 @@
 import os
 import time
+from util.config import first, second, third, fourth
+firstD = first
+secondD = second
+thirdD = third
+fourthD = fourth
+S = '-s '
+D = ' -d '
+C = ' --copy '
+os.system('python3 autorclone.py '+ C + S + tirdD + D + fourthD)
+
+time.sleep(15)
+os.system('rclone --config rclone-generated.conf size src001:')
+os.system('rclone --config rclone-generated.conf size dst001:')
+os.system('rclone --config rclone-generated.conf dedupe dst001:')
+time.sleep(120)
+
+
+
 #os.system('python3 autorclone.py --sync -s 0ACT5UpIf3zQMUk9PVA -d 0AJKePN7XU84hUk9PVA')
 #time.sleep(120)
 #os.system('python3 autorclone.py --sync -s 0AJKePN7XU84hUk9PVA -d 0ADCeN_vURI8gUk9PVA')
