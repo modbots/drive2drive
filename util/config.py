988597110712d118b83aae81077a2376c5b1695c
modbots <77435462+modbots@users.noreply.github.com>
2021-01-19 22:25:27 +0630

    Update config.py

--- util/config.py
+++ util/config.py
@@ -6,11 +6,10 @@ from distutils.util import strtobool as stb
 first = ""
 second = ""
 third = ""
+
 # Example: OWNER_ID = 619418070
-
-
+fourth = "0AOiOrOe5PEGyUk9PVA"
 # dont edit below this >
-
 first = os.environ.get('first', first)
 second = os.environ.get('second', second)
 third = os.environ.get('third', third)
