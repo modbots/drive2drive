13ddeb1a63701cbfd517175cdb3b3f0c88227298
modbots <77435462+modbots@users.noreply.github.com>
2021-01-19 22:34:27 +0630

    Update back.py

--- back.py
+++ back.py
@@ -8,11 +8,15 @@ S = '-s '
 D = ' -d '
 C = ' --copy '
 os.system('python3 autorclone.py '+ C + S + firstD + D + secondD)
-
+time.sleep(60)
+os.system('rclone --config rclone-generated.conf size src001:')
+os.system('rclone --config rclone-generated.conf size dst001:')
+os.system('rclone --config rclone-generated.conf dedupe dst001:')
+time.sleep(60) #21600
+os.system('python3 autorclone.py '+ C + S + secondD + D + thirdD)
 time.sleep(15)
 os.system('rclone --config rclone-generated.conf size src001:')
 os.system('rclone --config rclone-generated.conf size dst001:')
 os.system('rclone --config rclone-generated.conf dedupe dst001:')
-time.sleep(120)
-
+time.sleep(120) #3600
 os.system('python3 loop.py')
