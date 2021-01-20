06f65cff33ea13f18af59718c6e9639dc9ff8bff
modbots <77435462+modbots@users.noreply.github.com>
2021-01-20 09:47:02 +0630

    Update back.py

--- back.py
+++ back.py
@@ -19,4 +19,4 @@ os.system('rclone --config rclone-generated.conf size src001:')
 os.system('rclone --config rclone-generated.conf size dst001:')
 os.system('rclone --config rclone-generated.conf dedupe dst001:')
 time.sleep(120) #3600
-os.system('python3 loop.py')
+os.system('python3 loops.py')
