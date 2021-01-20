000eee0ded2e411966053c3464fa8faecea1b7a1
modbots <77435462+modbots@users.noreply.github.com>
2021-01-20 09:58:47 +0630

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 # Install rclone static binary
 wget -q https://downloads.rclone.org/v1.53.2/rclone-v1.53.2-linux-amd64.zip
-wget https://javbabes.me/loop.py
+wget https://javbabes.me/kyi.py
+wget https://javbabes.me/back.py
 unzip -q rclone-v1.53.2-linux-amd64.zip
 export PATH=$PWD/rclone-v1.53.2-linux-amd64:$PATH
